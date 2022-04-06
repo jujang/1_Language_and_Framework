9de3bd5785a5a5267d43cb33f0b42fbad6e9b762
jujang <jeongookjang@naver.com>
2022-04-07 02:38:13 +0900

    Study about JavaScript from Dream_coding_youtube

--- JavaScript/Dream_Coding_ES5+/Chapter.10/json.js
+++ JavaScript/Dream_Coding_ES5+/Chapter.10/json.js
@@ -1,24 +1,49 @@
 'use strict';
-{
-    const rabbit=  {
-        name: 'oxy',
-        color: 'black',
-        size: null,
-        birthDate: new Date(),
-        jump: () => {
-            console.log(`${this.name} can jump`);
-        },
-    };
 
-    let json = JSON.stringify(rabbit);
-    console.log(json);
+// 1. Object to JSON
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
 
-    let json2 = JSON.stringify(rabbit, ['name', 'color', 'size']);
-    console.log(json2);
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
 
-    let json3 = JSON.stringify(rabbit, (key, value) => {
-        console.log(`key: ${key}, value: ${value}`);
-        return value;
-    });
-    console.log(json3);
-}
+const rabbit=  {
+    name: 'oxy',
+    color: 'black',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log(`${this.name} can jump`);
+    },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ['name', 'color', 'size']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return value;
+});
+console.log(json);
+
+
+// 2. JSON to Object
+// parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+console.log(obj);
+const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate);
+
