533a324c47e757be2643649e576754f5a1d6e6dd
jujang <jeongookjang@naver.com>
2022-06-12 18:31:05 +0900

    Review about react from life_coding
    default settings & prop part

---
+++ JavaScript/life_coding/web2_javascript_react_review/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(props) {
+  const current = props.title;
+  return (    
+    <h1><a href="/">{current}</a></h1>
+  )
+}
+
+function Nav(props) {
+  const lis = [];
+  
+  for(let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+
+function Article(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      {props.body}
+    </div>
+  )
+}
+
+function App() {
+  const topics = [
+    {id:1, title:'html', body: 'html is...'},
+    {id:2, title:'css', body: 'css is...'},
+    {id:3, title:'js', body: 'js is...'}
+  ];
+  return (
+    <div>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello"></Article>
+    </div>
+  );
+}
+
+export default App;
