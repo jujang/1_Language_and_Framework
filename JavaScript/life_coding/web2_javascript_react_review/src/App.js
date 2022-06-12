027f5dee1539c8e399d43a9d004886dcec373b3c
jujang <jeongookjang@naver.com>
2022-06-12 22:30:59 +0900

    Review about react from life_coding
    state

--- JavaScript/life_coding/web2_javascript_react_review/src/App.js
+++ JavaScript/life_coding/web2_javascript_react_review/src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function Header(props) {
@@ -19,7 +20,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a>
     </li>)
   }
@@ -42,20 +43,36 @@ function Article(props) {
 }
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     {id:1, title:'html', body: 'html is...'},
     {id:2, title:'css', body: 'css is...'},
-    {id:3, title:'js', body: 'js is...'}
+    {id:3, title:'javascript', body: 'javascript is...'}
   ];
+    let content = null;
+  if(mode === 'WELCOME') {
+    content = <Article title='Welcome' body='Hello, WEB'></Article>
+  } else if(mode ==='READ') {
+    let title, body = null;
+    for(let i = 0; i < topics.length; i++){
+      if(topics[i].id === id ) {
+        title = topics[i].title;
+        body = topics[i].body
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        alert('Header');
+        setMode("WELCOME");
       }}></Header>
-      <Nav topics={topics} onChangeMode={(event)=>{
-        alert(event);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setId(_id);
+        setMode("READ");
       }}></Nav>
-      <Article title="Welcome" body="Hello"></Article>
+      {content}
     </div>
   );
 }
