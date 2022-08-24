a7c997414c9577d674291aeb8e3be3d7cd079523
jujang <jeongookjang@naver.com>
2022-08-24 17:51:23 +0900

    Study about Spring bacis from Codestates
    First setting

---
+++ Spring/Code_States/Section2/section2-week3/src/main/java/com/codestates/Section2Week3Application.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section2Week3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section2Week3Application.class, args);
+	}
+
+}
