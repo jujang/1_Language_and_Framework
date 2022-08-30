9d7697033c2d501e0aedfcaeeb6ce8c64485e409
jujang <jeongookjang@naver.com>
2022-08-30 11:19:37 +0900

    JAVA daily coding

---
+++ Java/Code_States/DailyCoding/Notepad/src/Q27_treeBFS_ref.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+public class Q27_treeBFS_ref {
+
+    public static void main(String[] args){
+
+        tree root = new tree("1");
+        tree rootChild1 = root.addChildNode(new tree("2"));
+        tree rootChild2 = root.addChildNode(new tree("3"));
+        tree leaf1 = rootChild1.addChildNode(new tree("4"));
+        tree leaf2 = rootChild1.addChildNode(new tree("5"));
+        ArrayList<String> output = bfs(root);
+        System.out.println(output); // --> ["1", "2", "3", "4", "5"]
+
+        leaf1.addChildNode(new tree("6"));
+        rootChild2.addChildNode(new tree("7"));
+        output = bfs(root);
+        System.out.println(output); // --> ["1", "2", "3", "4", "5", "7", "6"]
+
+    }
+
+    public static ArrayList<String> bfs(tree node) {
+
+        ArrayList<String> arraylist = new ArrayList<>();
+
+        ArrayList<tree> queue = new ArrayList<>();
+        queue.add(node);
+
+        while(queue.size() > 0){
+            tree curNode = queue.remove(0);
+            arraylist.add(curNode.getValue());
+
+            if(curNode.getChildrenNode() != null) queue.addAll(curNode.getChildrenNode());
+        }
+
+        return arraylist;
+    }
+
+    //아래 클래스의 내용은 수정하지 말아야 합니다.
+    public static class tree {
+        private String value;
+        private ArrayList<tree> children;
+
+        public tree(String data) {
+            this.value = data;
+            this.children = null;
+        }
+
+        public tree addChildNode(tree node) {
+            if(children == null) children = new ArrayList<>();
+            children.add(node);
+            return children.get(children.size() - 1);
+        }
+
+        public String getValue() {      //현재 노드의 데이터를 반환
+            return value;
+        }
+
+        public ArrayList<tree> getChildrenNode() {
+            return children;
+        }
+    }
+
+}
