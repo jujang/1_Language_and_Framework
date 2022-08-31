5f0053f9191d2eb8d1b8b4f66c6ec2fcb0059135
jujang <jeongookjang@naver.com>
2022-09-01 03:08:40 +0900

    Study about Spring MVC from Codestates
    Controller - setting

---
+++ Spring/Code_States/Section3/section3-week1/src/main/java/com/codestates/Section3Week1Application.java
@@ -0,0 +1,14 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3Week1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Section3Week1Application.class, args);
+	}
+
+}
