e28b6a511109e26e5ff7eb97479b1e3fb4897733
jujang <jeongookjang@naver.com>
2022-09-05 21:54:16 +0900

    JAVA_selfstudy

---
+++ Java/Self_Study/JAVA_Study/src/Collection/sortCollection.java
@@ -0,0 +1,19 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class sortCollection {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(1);
+        arrayList.add(30);
+        arrayList.add(2);
+        arrayList.add(10);
+
+        Collections.sort(arrayList);
+
+    }
+}
---
+++ Java/Self_Study/JAVA_Study/src/concateStringToHead.java
@@ -0,0 +1,12 @@
+public class concateStringToHead {
+    public static void main(String[] args){
+
+        String tailStr = "tail";
+        String str = "head";
+
+        str = str.concat(tailStr);
+
+        System.out.println(str);
+
+    }
+}
